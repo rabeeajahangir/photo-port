3d0cac48be5e5730ca1afdeedccb1cb439a00074
Rabeea Jahangir <rabeeajahangir@gmail.com>
2021-08-29 00:48:50 -0400

    added hooks and props to nav

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,37 +1,51 @@
 // __tests__/Nav.test.js with hard coded categories
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Nav from '..';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-afterEach(cleanup);
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
-describe('Nav component', () => {
-  it('renders', () => {
-    render(<Nav />);
-  });
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about">
+              About me
+            </a>
+          </li>
+          <li className="mx-2">
+            <span>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Nav />);
-    
-    expect(asFragment()).toMatchSnapshot();
-  });
-})
-
-describe('emoji is visible', () => {
-  it('inserts emoji into the h2', () => {
-  const { getByLabelText } = render(<Nav />);
-
-  expect(getByLabelText('camera')).toHaveTextContent('ðŸ“¸');
-  });
-})  
-
-describe('links are visible', () => {
-  it('inserts text into the links', () => {
-    const { getByTestId } = render(<Nav />);
-
-    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-    expect(getByTestId('about')).toHaveTextContent('About me');
-  });
-
-})
+export default Nav;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,42 +1,53 @@
-import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React, { useState } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 import React, { useEffect } from 'react';
 
-useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
 
-function Nav(props) {
-  const {
-    categories = [],
-    setCurrentCategory,
-    currentCategory,
-  } = props;
+useEffect(() => {
+  document.title = capitalizeFirstLetter(currentCategory.name);
+}, [currentCategory]);
+
+
+
+function Nav() {
+const [currentCategory, setCurrentCategory] = useState(categories[0]);
+const [categories] = useState([
+  {
+    name: 'commercial',
+    description: 'Photos of grocery stores, food trucks, and other commercial projects',
+  },
+  { name: 'portraits', description: 'Portraits of people in my life' },
+  { name: 'food', description: 'Delicious delicacies' },
+  { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+]);
 
   return (
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">
+            <a
+              href="#about"
+            >
               About me
             </a>
           </li>
-          <li className="mx-2">
+          <li>
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li
-              className={`mx-1 ${
+            <li className={`mx-1 ${
                 currentCategory.name === category.name && 'navActive'
-                }`}
-              key={category.name}
-            >
+                }`} key={category.name}>
               <span
                 onClick={() => {
                   setCurrentCategory(category)
